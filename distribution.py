22b448212896e8cfb61c9b9f6b101e6531960292
erpanMac <erpan14ar@gmail.com>
2017-08-29 16:20:26 +0200

    rename and polishing

--- distribution.py
+++ distribution.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from optparse import OptionParser
-from dist_plot import files_distributor
+from files_distributor import files_distributor
 
 
 description = "Distribute a list of files with different sizes to a list of nodes with different capacities"
@@ -15,19 +15,11 @@ parser.add_option("-o", "--output",help="output file name, if not given, output 
 parser.add_option("-p", "--plot",help="plot nodes usage",
                   action="store_true",  default=False, dest="plot_dist")
 (options, args) = parser.parse_args()
-if options.files_filename is None or options.nodes_filename is None:   # if filename is not given
+if not options.files_filename or not options.nodes_filename:   # if filename is not given
     parser.error('Check if all the file names are given!')
 
-# files, nodes = parse_files(options.files_filename,options.nodes_filename)
-
-# distribute(files,nodes)
-
-# if options.plot_dist:
-#     plot_bar(files,nodes)
-
-# print_output(files,nodes,options.out_files)
-
 distribute = files_distributor(options.files_filename,options.nodes_filename)
+
 distribute.distribute()
 if options.plot_dist:
 	distribute.plot_bar()
--- files_distributor.py
+++ files_distributor.py
@@ -1,7 +1,11 @@
+import pandas as pd
+import numpy as np
 class files_distributor:
 
    def __init__(self,files_filename,nodes_filename):
        self.files,  self.nodes = self.parse_files(files_filename,nodes_filename)
+       if self.nodes.empty:
+          raise RuntimeError("Nodes are empty")      
 
 
    def parse_files(self,files_filename,nodes_filename):
@@ -10,15 +14,13 @@ class files_distributor:
        Adding 'notAllocated' and 'AssignedNode' columns in files to indicate state of each file and the node it has assigned to.
        Adding 'files' columns to indicate a list of files it has been assigned to.
        '''
-       import pandas as pd
-       import numpy as np
        #read two files as pandas Dataframes
        files = pd.read_csv(files_filename,sep=' ',\
            header=None,comment='#',names=['files','size'],\
            dtype={'files':str,'size':np.int32})
        nodes = pd.read_csv(nodes_filename,sep=' ',\
            header=None,comment='#',names=['nodes','capacity'],\
-           dtype={'files':str,'size':np.int32})
+           dtype={'nodes':str,'capacity':np.int32})
        #initialize them with 
        files['notAllocated'] = True
        files['AssignedNode'] = ''
@@ -38,11 +40,12 @@ class files_distributor:
        than the largest available node capacity, Mark as NULL. Repeat until all 
        files are allocated or marked as NULL. 
        '''
-       files = self.files.copy()
-       nodes = self.nodes.copy()
+       files = self.files
+       nodes = self.nodes
        files.sort_values('size',inplace=True,ascending=False)
        while any(files['notAllocated']): #repeat if any of the files is not allocated.
            frest = files[files['notAllocated']].copy()
+           frest.sort_values('size',inplace=True,ascending=False)
            nodes.sort_values('space_left',inplace=True,ascending=False) #sort the nodes by capacity
            for i in range(min(sum(frest['notAllocated']),len(nodes['space_left']))): #for the shorter of two lists
                if nodes['space_left'].iloc[i] >= frest['size'].iloc[i]: #if there is enough available space
@@ -58,41 +61,39 @@ class files_distributor:
                    files.set_value(frest.index[i],'notAllocated',False)
                    files.set_value(frest.index[i],'AssignedNode','NULL')
                    break #break to resort the node, and repeat.
-       self.files_done = files.copy()
-       self.nodes_done = nodes.copy()
 
    def plot_bar(self):
        '''
        plot the usage of each node containing blocks of bar for the files the node has been assigned.
        '''
        import matplotlib.pyplot as plt
-       import pandas as pd
        #pivot table files vs nodes with file size as elements
-       files_nodes = pd.pivot_table(self.files_done[self.files_done['AssignedNode'] != 'NULL'], values='size', index='AssignedNode', columns='files')
-       self.nodes_done.set_index('nodes',inplace=True)
+       files_nodes = pd.pivot_table(self.files[self.files['AssignedNode'] != 'NULL'], values='size', index='AssignedNode', columns='files')
+       self.nodes.set_index('nodes',inplace=True)
        #append space left on each node
-       total_fn = pd.concat([files_nodes,self.nodes_done['space_left']],axis=1)
+       total_fn = pd.concat([files_nodes,self.nodes['space_left']],axis=1)
        #make stacked bar plot
-       ax = total_fn.plot.bar(stacked=True,title='Files distribution among nodes',cmap='nipy_spectral_r')
+       n_col=int((len(self.files['files'])+14)/14)
+       ax = total_fn.plot.bar(figsize=(5+n_col*2,5), stacked=True,title='Files distribution among nodes',cmap='nipy_spectral_r')
        ax.set_xlabel("Nodes");ax.set_ylabel("Capacity")
        #make legend for the file names, ncol will increase accordingly with the number of files
-       ax.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.2,ncol=int((len(self.files_done['files'])+14)/14))
+       ax.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.2,ncol=n_col)
        plt.tight_layout(rect=(0,0,0.7,1))
        plt.show()
 
    def print_output(self,out_files):
        from tabulate import tabulate
-       self.files_done.set_index('files',inplace=True)    
-       self.nodes_done.sort_values('capacity',inplace=True,ascending=False)	
+       self.files.set_index('files',inplace=True)    
+       self.nodes.sort_values('capacity',inplace=True,ascending=False)	
 
        if out_files:
            #output files
-           self.files_done.to_csv(out_files,columns=['AssignedNode'],header=False,sep=' ')
+           self.files.to_csv(out_files,columns=['AssignedNode'],header=False,sep=' ')
            #output nodes
-           #self.nodes_done.to_csv('nodes.out',header=False,sep=' ',index=False)
+           #self.nodes.to_csv('nodes.out',header=False,sep=' ',index=False)
        else:
-           print tabulate(self.files_done[['size','AssignedNode']], headers='keys', tablefmt='fancy_grid')
-           #print tabulate(self.nodes_done, headers='keys', tablefmt='fancy_grid')
+           print tabulate(self.files[['size','AssignedNode']], headers='keys', tablefmt='fancy_grid')
+           print tabulate(self.nodes, headers='keys', tablefmt='fancy_grid')
 
        
 
